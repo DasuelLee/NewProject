d240a91e6879ed5f2d2a639d529ac99fc4d8bd2d
DasuelLee <dasuel.lee@gmail.com>
2016-07-22 11:45:43 +0900

    upload test

---
+++ SuperExam.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.superex;
+
+public class SuperExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Truck t = new Truck();
+
+	}
+
+}
